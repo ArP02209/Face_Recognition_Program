0222864a01dd8653efd9a03274f6ae56a8cb4dc6
ArP02209 <72129899+ArP02209@users.noreply.github.com>
2021-06-22 13:18:27 +0530

    Add files via upload

---
+++ tpvars.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+base_url="https://api.telegram.org/bot1885886947:AAEglhZ_uzCDRUbS5YhkOSXeaIrciPx0sA8/sendMessage?chat_id=-1001350093974&text='Face detected and image recognized sucessfully!!' "
+
+
+# In[ ]:
+
+
+
+
