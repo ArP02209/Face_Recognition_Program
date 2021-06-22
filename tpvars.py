ba556dd2ca12cb0a01094d9008826c14fff5800e
ArP02209 <72129899+ArP02209@users.noreply.github.com>
2021-06-22 13:20:17 +0530

    Update tpvars.py

--- tpvars.py
+++ tpvars.py
@@ -4,7 +4,7 @@
 # In[ ]:
 
 
-base_url="https://api.telegram.org/bot1885886947:AAEglhZ_uzCDRUbS5YhkOSXeaIrciPx0sA8/sendMessage?chat_id=-1001350093974&text='Face detected and image recognized sucessfully!!' "
+base_url="https://api.telegram.org/bot<bot_token>/sendMessage?chat_id=<id>&text='Face detected and image recognized sucessfully!!' "
 
 
 # In[ ]:
